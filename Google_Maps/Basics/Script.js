752ce8094a6c0b72b0fd0dcc95a254332566475f
Stefanpu13 <stefan.uzunov@amexis.net>
2014-05-27 19:18:48 +0300

    Init Google maps project

---
+++ Google_Maps/Basics/Script.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="Scripts/gmap3.js" />
+/// <reference path="google-maps-3-vs-1-0.js" />
+var maps = google.maps;
+
+function initialize() {
+    var mapOptions = {
+        center: new maps.LatLng(42.239818, 25.300602),
+        zoom: 9,
+        mapTypeId:maps.MapTypeId.SATELLITE
+    };
+    var map = new maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
+
+    alert(maps.Symbol);
+
+    var marker = new maps.Marker({
+        title: "First Marker",
+        map: map,
+        position: map.getCenter()
+    });
+
+    maps.event.addListener(map, 'click', function (e) {
+        //var map = this;
+
+        var marker = new maps.Marker({
+            map: map,
+            position: e.latLng,
+            icon: new maps.Symbol()
+        });
+    });
+}
+
+
+
+
+maps.event.addDomListener(window, 'load', initialize);
+
+//maps.event.addDomListener(window, 'load', initialize);
---
+++ Google_Maps/Basics/gmapvsdoc3.v1/ReadMe.txt
@@ -0,0 +1,22 @@
+﻿Quick Start:
+
+* Place the files google-maps-3-vs-1-0.js and google-maps-3-vs-1-0-vsdoc.js in the same directory.
+
+* Add a script tag referencing google-maps-3-vs-1-0.js AFTER the google maps 3 api script tag.
+
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+
+    <!-- deploy this script AFTER the maps api-->
+
+    <script src="scripts/google-maps-3-vs-1-0.js" type="text/javascript"></script>
+
+* In standalone script files add a <reference> tag to google-maps-3-vs-1-0.js.
+
+	/// <reference path="google-maps-3-vs-1-0.js" />
+
+
+
+
+
+
+TODO: implement full coverage tests
